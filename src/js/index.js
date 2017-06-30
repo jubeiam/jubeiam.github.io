46a1f57625928b0a2b3042b7bb71855e43d96c76
Leszek Stęczniewski <jubeiam@gmail.com>
2017-06-30 22:18:49 +0200

    rm tw

--- src/js/index.js
+++ src/js/index.js
@@ -4,7 +4,7 @@ import VueRouter from 'vue-router'
 import Vivecard from './../components/vivecard.vue'
 import Drawer from './../components/drawer.vue'
 import Board from './../components/2048/board.vue'
-import Twitch from './../components/twitch/page.vue'
+// import Twitch from './../components/twitch/page.vue'
 import Quiz from './../components/quiz/page.vue'
 
 
@@ -15,7 +15,7 @@ const router = new VueRouter({
 	routes: [
 		{path: '/vivecard', component: Vivecard}
 		,{path: '/2048', component: Board}
-		,{path: '/twitch', component: Twitch}
+		// ,{path: '/twitch', component: Twitch}
 		,{path: '/quiz', component: Quiz}
 		,{path: '*', redirect: '/vivecard'}
 	]
